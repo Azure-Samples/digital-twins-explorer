6e80b7a954902a7adcfa01bc4722e564a08878b0
Jamie Wilbraham <jamiewilbraham@users.noreply.github.com>
2020-10-09 05:29:43 +1300

    Adding updated validation and multi-instance support (#33)
    Co-authored-by: Jamie Wilbraham <jamie@zarmada.com>

--- client/src/components/ConfigurationFormComponent/ConfigurationFormComponent.js
+++ client/src/components/ConfigurationFormComponent/ConfigurationFormComponent.js
@@ -2,9 +2,10 @@
 // Licensed under the MIT license.
 
 import React, { Component } from "react";
-import { TextField, DefaultButton, PrimaryButton, FocusZone, FocusZoneTabbableElements } from "office-ui-fabric-react";
+import { TextField, DefaultButton, PrimaryButton, FocusZone, FocusZoneTabbableElements, Dropdown, Icon } from "office-ui-fabric-react";
 import ModalComponent from "../ModalComponent/ModalComponent";
 import { eventService } from "../../services/EventService";
+import { settingsService } from "../../services/SettingsService";
 
 import "./ConfigurationFormComponent.scss";
 
@@ -16,12 +17,19 @@ export class ConfigurationFormComponent extends Component {
       showModal: false,
       appClientId: "",
       appTenantId: "",
-      appAdtUrl: ""
+      appAdtUrl: "",
+      environmentOptions: []
     };
+    this.environments = settingsService.environments;
   }
 
   componentDidMount() {
     eventService.subscribeConfigure(evt => this.loadConfigurationSettings(evt));
+    if (this.environments) {
+      this.setState({
+        environmentOptions: this.environments.map(env => env.name)
+      });
+    }
   }
 
   loadConfigurationSettings = evt => {
@@ -37,10 +45,50 @@ export class ConfigurationFormComponent extends Component {
       appTenantId: this.state.appTenantId,
       appAdtUrl: this.state.appAdtUrl
     };
-    if (config.appClientId && config.appTenantId && config.appAdtUrl) {
+
+    if (this.validateConfig(config)) {
+      this.saveEnvironment(config);
       eventService.publishConfigure({ type: "end", config });
       this.resetModalState();
     }
+  }
+
+  saveEnvironment = config => {
+    const { environmentOptions } = this.state;
+    const { appAdtUrl } = config;
+    if (this.environments) {
+      const environment = this.environments.find(env => env.name === appAdtUrl);
+      if (environment) {
+        environment.config = config;
+      } else {
+        this.environments.push({ name: appAdtUrl, config });
+        environmentOptions.push(appAdtUrl);
+      }
+    } else {
+      this.environments = [ { name: appAdtUrl, config } ];
+      environmentOptions.push(appAdtUrl);
+    }
+    settingsService.environments = this.environments;
+  }
+
+  validateConfig = config => {
+    if (!config.appClientId || !config.appTenantId || !config.appAdtUrl) {
+      eventService.publishError("*** All fields are required.");
+      return false;
+    }
+
+    if (!config.appAdtUrl.startsWith("https")) {
+      eventService.publishError("*** ADT URL must start with ‘https’.");
+      return false;
+    }
+
+    const regexp = /^(https):\/\/[\w-]+.api.[\w-.]+.[\w-.]+digitaltwins[\w-.]+/gm;
+    if (!regexp.test(config.appAdtUrl)) {
+      eventService.publishError("*** ADT URL must match the format 'https://<name>.api.<dc>.<domain>'.");
+      return false;
+    }
+
+    return true;
   }
 
   closeConfigurationSettings = e => {
@@ -58,6 +106,44 @@ export class ConfigurationFormComponent extends Component {
     });
   }
 
+  onSelectedEnvironmentChange = (e, i) => {
+    if (this.environments) {
+      const environment = this.environments.find(env => env.name === i.key);
+      if (environment) {
+        this.setState({ appAdtUrl: environment.name, ...environment.config });
+      }
+    }
+  }
+
+  onEnvironmentNameChange = e => {
+    this.setState({ appAdtUrl: e.target.value });
+  }
+
+  onRemoveEnvironmentClick = item => {
+    const { environmentOptions, appAdtUrl } = this.state;
+    const filteredOptions = environmentOptions.filter(option => option !== item);
+    this.environments = this.environments.filter(env => env.name !== item);
+    settingsService.environments = this.environments;
+    this.setState({ environmentOptions: filteredOptions });
+    if (item === appAdtUrl) {
+      this.resetModalState(true);
+    }
+  }
+
+  onRenderOption = item => (
+    <div className="dropdown-option">
+      <span>{item.text}</span>
+      <Icon
+        className="close-icon"
+        iconName="ChromeClose"
+        aria-hidden="true"
+        onClick={() => this.onRemoveEnvironmentClick(item.key)}
+        aria-label={`Remove ${item} environment`}
+        role="button"
+        title="Remove environment"
+        tabIndex="0" />
+    </div>)
+
   onAppClientIdChange = evt => this.setState({ appClientId: evt.target.value })
 
   onAppTenantIdChange = evt => this.setState({ appTenantId: evt.target.value })
@@ -71,7 +157,7 @@ export class ConfigurationFormComponent extends Component {
   })
 
   render() {
-    const { appClientId, appTenantId, appAdtUrl, showModal } = this.state;
+    const { appClientId, appTenantId, appAdtUrl, showModal, environmentOptions } = this.state;
 
     return (
       <ModalComponent
@@ -80,12 +166,22 @@ export class ConfigurationFormComponent extends Component {
         <FocusZone handleTabKey={FocusZoneTabbableElements.all} isCircularNavigation defaultActiveElement="#appClientIdField">
           <form onSubmit={this.saveConfigurationsSettings}>
             <h2 className="heading-2">Sign In</h2>
+            <div className="select-settings">
+              <Dropdown
+                placeholder="Selected Environment"
+                options={environmentOptions.map(env => ({ key: env, text: env }))}
+                onRenderOption={this.onRenderOption}
+                styles={{
+                  dropdown: { width: "100%" }
+                }}
+                onChange={this.onSelectedEnvironmentChange} />
+              <TextField required id="appAdtUrlField" label="ADT URL" className="configuration-input"
+                styles={this.getStyles} value={appAdtUrl} onChange={this.onAppAdtUrlChange} />
+            </div>
             <TextField required id="appClientIdField" label="Client ID" className="configuration-input"
               styles={this.getStyles} value={appClientId} onChange={this.onAppClientIdChange} />
             <TextField required id="appTenantIdField" label="Tenant ID" className="configuration-input"
               styles={this.getStyles} value={appTenantId} onChange={this.onAppTenantIdChange} />
-            <TextField required id="appAdtUrlField" label="ADT URL" className="configuration-input"
-              styles={this.getStyles} value={appAdtUrl} onChange={this.onAppAdtUrlChange} />
             <p>Configuration data is saved in local storage.</p>
             <div className="btn-group">
               <PrimaryButton type="submit" className="modal-button save-button" onClick={this.saveConfigurationsSettings}>
