b6ac34a3fcb5b3ca7a544d5bfecfff1d78c795db
Alex Karcher <alkarche@microsoft.com>
2021-02-22 18:21:15 -0800

    Converted promises to async

--- client/src/services/RBACService.js
+++ client/src/services/RBACService.js
@@ -6,100 +6,95 @@ import { TWIN_DATA_OWNER_RBAC_ID } from "./Constants";
 
 class RBACService {
 
-  async addReaderRBAC() {
+  uuidv4() {
     // Function to generate a random GUID
-    function uuidv4() {
-      return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, c => {
-        /* eslint no-bitwise: [2, { allow: ["|"] }] */
-        const r = Math.random() * 16 | 0;
-        const v = c === "x" ? r : (r | 0x3 | 0x8);
-        return v.toString(16);
-      });
-    }
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, c => {
+      /* eslint no-bitwise: [2, { allow: ["|"] }] */
+      const r = Math.random() * 16 | 0;
+      const v = c === "x" ? r : (r | 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
 
-    // Get our current twins instance from settings
-    const { appAdtUrl } = await configService.getConfig();
-    const requestParams = {
-      "appName": appAdtUrl.split(".api.")[0].substring(8)
-    };
-    // Get the user's principal ID
-    const requestOptions = {
+  async getIdentity(path) {
+    const userOptions = {
       method: "GET",
       headers: {
         "x-adt-host": "graph.microsoft.com"
       }
     };
-    return fetch("http://localhost:3000/api/proxy/Graph/v1.0/me", requestOptions)
-      .then(response => response.json())
-      .then(result => {
-        requestParams.userId = result.id;
-        return requestParams;
-      })
-      .then(userParams => {
-        // Get subscriptions from logged in user
-        const userOptions = {
-          method: "GET",
-          headers: {
-            "x-adt-host": "management.azure.com"
-          }
-        };
-        return fetch("http://localhost:3000/api/proxy/RBAC/subscriptions?api-version=2020-01-01", userOptions)
-          .then(response => response.json())
-          .then(result => {
-            userParams.subscriptions = result;
-            return userParams;
-          });
-      })
-      .then(subscriptionParams => {
-        // Loop through our subscriptions to get the twins instance
-        const subscriptionOptions = {
-          method: "GET",
-          headers: {
-            "x-adt-host": "management.azure.com"
-          }
-        };
+    const results = await fetch(`http://localhost:3000/api/proxy/Graph/${path}`, userOptions);
+    return await results.json();
+  }
 
-        const subscriptionRequests = [];
-        for (const x of subscriptionParams.subscriptions.value) {
-          subscriptionRequests.push(
-            fetch(`http://localhost:3000/api/proxy/RBAC${x.id}/providers/Microsoft.DigitalTwins/digitalTwinsInstances?api-version=2020-10-31`, subscriptionOptions)
-              .then(response => response.json())
-              .then(result => {
-                if ("value" in result && result.value !== false) {
-                  for (const value in result.value) {
-                    if (result.value[value].name.toLowerCase() === subscriptionParams.appName.toLowerCase()) {
-                      subscriptionParams.ARMId = result.value[value].id;
-                      return subscriptionParams;
-                    }
-                  }
-                }
-                return 0;
-              })
-              .then(roleParams => {
-                if (roleParams === 0) {
-                  return false;
-                }
-                // POST request using fetch with set headers
-                const roleOptions = {
-                  method: "PUT",
-                  headers: {
-                    "Content-Type": "application/json",
-                    "x-adt-host": "management.azure.com"
-                  },
-                  body: JSON.stringify({
-                    "properties": {
-                      "principalId": `${roleParams.userId}`,
-                      "roleDefinitionId": `${roleParams.ARMId}/providers/Microsoft.Authorization/roleDefinitions/${TWIN_DATA_OWNER_RBAC_ID}`
-                    }
-                  })
-                };
-                return fetch(`http://localhost:3000/api/proxy/RBAC${roleParams.ARMId}/providers/Microsoft.Authorization/roleAssignments/${uuidv4()}?api-version=2020-04-01-preview`, roleOptions);
-              }
-              )
-          );
+  async getRBAC(path) {
+    const userOptions = {
+      method: "GET",
+      headers: {
+        "x-adt-host": "management.azure.com"
+      }
+    };
+    const results = await fetch(`http://localhost:3000/api/proxy/RBAC/${path}`, userOptions);
+    return await results.json();
+  }
+
+  async postRBACRole(userPrincipalId, twinARMId) {
+    const userOptions = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        "x-adt-host": "management.azure.com"
+      },
+      body: JSON.stringify({
+        "properties": {
+          "principalId": `${userPrincipalId}`,
+          "roleDefinitionId": `${twinARMId}/providers/Microsoft.Authorization/roleDefinitions/${TWIN_DATA_OWNER_RBAC_ID}`
         }
-        return Promise.all(subscriptionRequests);
-      });
+      })
+    };
+    return await fetch(`http://localhost:3000/api/proxy/RBAC${twinARMId}/providers/Microsoft.Authorization/roleAssignments/${this.uuidv4()}?api-version=2020-04-01-preview`, userOptions);
+  }
+
+  async postTwinsAccess(appName, subId, userPrincipalId) {
+    // Get the twins from a given subscription
+    const result = await this.getRBAC(`${subId}/providers/Microsoft.DigitalTwins/digitalTwinsInstances?api-version=2020-10-31`);
+
+    // Check if our twin is in the list of all twins in that subscription
+    let ARMId = null;
+    if ("value" in result && result.value !== false) {
+      for (const value in result.value) {
+        if (result.value[value].name.toLowerCase() === appName.toLowerCase()) {
+          ARMId = result.value[value].id;
+        }
+      }
+    }
+    if (ARMId === null) {
+      return false;
+    }
+    return await this.postRBACRole(userPrincipalId, ARMId);
+  }
+
+  async addReaderRBAC() {
+    // Get our current twins instance from settings
+    const { appAdtUrl } = await configService.getConfig();
+    const requestParams = {
+      "appName": appAdtUrl.split(".api.")[0].substring(8)
+    };
+
+    // Get the user's principal ID
+    const me = await this.getIdentity("v1.0/me");
+    requestParams.userId = me.id;
+
+    // Get the user's logged in subscriptions
+    const subscriptions = await this.getRBAC("subscriptions?api-version=2020-01-01");
+    requestParams.subscriptions = subscriptions;
+
+    // Loop through our subscriptions to get the twins instance
+    const subscriptionRequests = [];
+    for (const x of requestParams.subscriptions.value) {
+      subscriptionRequests.push(this.postTwinsAccess(requestParams.appName, x.id, requestParams.userId));
+    }
+    return Promise.all(subscriptionRequests);
   }
 
 }
