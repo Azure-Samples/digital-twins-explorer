09b45bd93ef8820274ec9b22d843ec485c6ef8c2
Selim Diaz Araya <selim@zarmada.com>
2021-03-01 20:30:19 +0000

    Avoid exception with url config

--- client/src/components/ConfigurationFormComponent/ConfigurationFormComponent.js
+++ client/src/components/ConfigurationFormComponent/ConfigurationFormComponent.js
@@ -82,20 +82,24 @@ export class ConfigurationFormComponent extends Component {
 
   validateConfig = config => {
     if (!config.appAdtUrl) {
-      eventService.publishError("All fields are required.");
+      eventService.publishError({
+        customMessage: "All fields are required."
+      });
       return false;
     }
 
     if (!config.appAdtUrl.startsWith("https")) {
-      eventService.publishError("ADT URL must start with ‘https’.");
+      eventService.publishError({
+        customMessage: "ADT URL must start with ‘https’."
+      });
       return false;
     }
 
     const regexp = /^(https):\/\/[\w-]+.api.[\w-.]+.[\w-.]+digitaltwins[\w-.]+/gm;
     if (!regexp.test(config.appAdtUrl)) {
-      eventService.publishError(
-        "ADT URL must match the format 'https://<name>.api.<dc>.<domain>'."
-      );
+      eventService.publishError({
+        customMessage: "ADT URL must match the format 'https://<name>.api.<dc>.<domain>'."
+      });
       return false;
     }
 
--- client/src/components/ErrorMessageComponent/ErrorMessage.js
+++ client/src/components/ErrorMessageComponent/ErrorMessage.js
@@ -39,7 +39,7 @@ export class ErrorMessageComponent extends Component {
 
       this.setState({
         errorMessage,
-        stackErrorMessage: exc.stack.replace(/\n/g, "<br>").replace(/ /gi, "&nbsp"),
+        stackErrorMessage: exc.stack ? exc.stack.replace(/\n/g, "<br>").replace(/ /gi, "&nbsp") : null,
         showModal: true
       });
     });
@@ -59,9 +59,11 @@ export class ErrorMessageComponent extends Component {
           <h2 className="heading-2"><span>!</span>Error</h2>
           <p>{errorMessage}</p>
           <p>Find more infomation on how to resolve issues like this here: <a href={MORE_INFORMATION_LINK} target="_blank" rel="noopener noreferrer">{MORE_INFORMATION_LINK}</a></p>
-          <div className="error-description">
-            <p dangerouslySetInnerHTML={{ __html: stackErrorMessage }} />
-          </div>
+          {stackErrorMessage && (
+            <div className="error-description">
+              <p dangerouslySetInnerHTML={{ __html: stackErrorMessage }} />
+            </div>
+          )}
           <div className="btn-group">
             <DefaultButton className="modal-button close-button" onClick={this.close}>Close</DefaultButton>
           </div>
