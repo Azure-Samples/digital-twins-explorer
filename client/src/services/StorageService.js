36b7069730e33350778fddba516fdba6150ff11c
Matt Darsney <matdar@microsoft.com>
2023-01-11 11:36:02 -0800

    Handle malformed configuration storage key (#297)
    * gracefully handle malformed configuration key
    
    * handle configuration key specifically
    
    * add comment

--- client/src/services/StorageService.js
+++ client/src/services/StorageService.js
@@ -5,7 +5,20 @@ class StorageService {
 
   getLocalStoragePrimitive = name => localStorage.getItem(name)
 
-  getLocalStorageObject = name => JSON.parse(localStorage.getItem(name))
+  /* In the event that a local storage key is malformed (not JSON parsable),
+  we gracefully fallback. If it is for configuration,
+  we provide an appAdtUrl that is empty string to avoid object destructuring
+  issues on code blocks dependent on that specific structure */
+  getLocalStorageObject = name => {
+    try {
+      return JSON.parse(localStorage.getItem(name));
+    } catch (e) {
+      if (name === "configuration") {
+        return {appAdtUrl: ""};
+      }
+      return {};
+    }
+  }
 
   setLocalStorageObject = (name, dataObj) => localStorage.setItem(name, JSON.stringify(dataObj))
 
