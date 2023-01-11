b707b97caadd72abe2745b4f08702401cd115129
Matt Darsney <matdar@microsoft.com>
2023-01-11 11:34:35 -0800

    add comment

--- client/src/services/StorageService.js
+++ client/src/services/StorageService.js
@@ -5,6 +5,10 @@ class StorageService {
 
   getLocalStoragePrimitive = name => localStorage.getItem(name)
 
+  /* In the event that a local storage key is malformed (not JSON parsable),
+  we gracefully fallback. If it is for configuration,
+  we provide an appAdtUrl that is empty string to avoid object destructuring
+  issues on code blocks dependent on that specific structure */
   getLocalStorageObject = name => {
     try {
       return JSON.parse(localStorage.getItem(name));
